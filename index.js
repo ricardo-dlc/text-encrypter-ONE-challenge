69d80444dce4585399ee230efd796f9cffa26e19
Ricardo de la Cruz <the_phantom_racer@hotmail.com>
2024-03-20 19:16:58 -0500

    Add decrypt function

--- index.js
+++ index.js
@@ -4,25 +4,45 @@
 // La letra "o" es convertida para "ober"
 // La letra "u" es convertida para "ufat"
 
-const encryptRules = {
-    a: "ai",
-    e: "enter",
-    i: "imes",
-    o: "ober",
-    u: "ufat"
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const encryptRules = {
+        a: "ai",
+        e: "enter",
+        i: "imes",
+        o: "ober",
+        u: "ufat"
+    }
 
-const encrypt = () => {
     const result = document.getElementById('encryption-result');
     const textArea = document.getElementById('text-to-encrypt');
-    const textToEncrypt = textArea.value;
 
-    const encryptedText = Array.from(textToEncrypt).reduce((text, char) => {
-        const result = encryptRules[char] || char;
-        return text + result;
-    }, "");
+    const encrypt = () => {
+        const textToEncrypt = textArea.value;
 
-    textArea.value = '';
+        const encryptedText = Array.from(textToEncrypt).reduce((text, char) => {
+            const result = encryptRules[char] || char;
+            return text + result;
+        }, "");
 
-    result.innerHTML = encryptedText;
-}
+        textArea.value = '';
+
+        result.innerHTML = encryptedText;
+    }
+
+    const decrypt = () => {
+        let decryptedText = textArea.value;
+
+        Object.entries(encryptRules).forEach(([key, value]) => {
+            decryptedText = decryptedText.replaceAll(value, key);
+        });
+
+        textArea.value = '';
+
+        result.innerHTML = decryptedText;
+    }
+
+    document.getElementById('encrypt-button').addEventListener('click', encrypt);
+    document.getElementById('decrypt-button').addEventListener('click', decrypt);
+});
+
+
